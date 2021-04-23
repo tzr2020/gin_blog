663b851409d004161ae8db7ad03e93261e53a4d2
tzr2020 <zhonziran@gmail.com>
2021-04-23 18:21:07 +0800

    错误处理模块、路由接口

--- routes/router.go
+++ routes/router.go
@@ -1,9 +1,8 @@
 package routes
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	v1 "github.com/tzr2020/gin_blog/api/v1"
 	"github.com/tzr2020/gin_blog/utils"
 )
 
@@ -11,13 +10,17 @@ func InitRouter() *gin.Engine {
 	gin.SetMode(utils.AppMode)
 	r := gin.Default()
 
-	v1 := r.Group("api/v1")
+	rgv1 := r.Group("api/v1")
 	{
-		v1.GET("hello", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"msg": "ok",
-			})
-		})
+		// 用户模块的路由接口
+		rgv1.POST("user/add", v1.AddUser)
+		rgv1.GET("users", v1.GetUsers)
+		rgv1.PUT("user/:id", v1.EditUser)
+		rgv1.DELETE("user/:id", v1.DeleteUser)
+
+		// 文章模块的路由接口
+
+		// 分类模块的路由接口
 	}
 
 	return r
