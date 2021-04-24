86e04e1e50a87b542847faa3fc5ec1cf6d5bb259
tzr2020 <zhonziran@gmail.com>
2021-04-24 20:18:32 +0800

    修改jwt中间件

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -16,56 +16,70 @@ var code int
 
 type MyClaims struct {
 	Username string `json:"username"`
-	Password string `json:"password"`
 	jwt.StandardClaims
 }
 
 // jwt中间件
 func JwtMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		tokenHeader := c.Request.Header.Get("Authorizathion")
+		tokenHeader := c.Request.Header.Get("Authorization")
 
 		if tokenHeader == "" {
 			code = errmsg.ERROR_TOKEN_EXIST
+			c.JSON(http.StatusOK, gin.H{
+				"code":    code,
+				"message": errmsg.GetErrMsg(code),
+			})
 			c.Abort()
+			return
 		}
 		checkToken := strings.SplitN(tokenHeader, " ", 2)
 		if len(checkToken) != 2 && checkToken[0] != "Bearer" {
 			code = errmsg.ERROR_TOKEN_TYPE
+			c.JSON(http.StatusOK, gin.H{
+				"code":    code,
+				"message": errmsg.GetErrMsg(code),
+			})
 			c.Abort()
+			return
 		}
-		key, Tcode := CheckToken(checkToken[1])
-		if Tcode == errmsg.ERROR {
+		key, tCode := CheckToken(checkToken[1])
+		if tCode == errmsg.ERROR {
 			code = errmsg.ERROR_TOKEN_WRONG
+			c.JSON(http.StatusOK, gin.H{
+				"code":    code,
+				"message": errmsg.GetErrMsg(code),
+			})
 			c.Abort()
+			return
 		}
 		if time.Now().Unix() > key.ExpiresAt {
 			code = errmsg.ERROR_TOKEN_RUNTIME
+			c.JSON(http.StatusOK, gin.H{
+				"code":    code,
+				"message": errmsg.GetErrMsg(code),
+			})
 			c.Abort()
+			return
 		}
 
-		c.JSON(http.StatusOK, gin.H{
-			"code":    code,
-			"message": errmsg.GetErrMsg(code),
-		})
 		c.Set("username", key.Username)
 		c.Next()
 	}
 }
 
 // 生成token
-func SetToken(username string, password string) (string, int) {
+func SetToken(username string) (string, int) {
 	expireTime := time.Now().Add(10 * time.Hour)
 	SetClaims := MyClaims{
 		username,
-		password,
 		jwt.StandardClaims{
 			ExpiresAt: expireTime.Unix(),
 			Issuer:    "ginblog",
 		},
 	}
 
-	reqClaims := jwt.NewWithClaims(jwt.SigningMethodES256, SetClaims)
+	reqClaims := jwt.NewWithClaims(jwt.SigningMethodHS256, SetClaims)
 	token, err := reqClaims.SignedString(JwtKey)
 	if err != nil {
 		return "", errmsg.ERROR
@@ -78,7 +92,7 @@ func CheckToken(token string) (*MyClaims, int) {
 	setToken, _ := jwt.ParseWithClaims(token, &MyClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return JwtKey, nil
 	})
-	if key, code := setToken.Claims.(*MyClaims); code && setToken.Valid {
+	if key, _ := setToken.Claims.(*MyClaims); setToken.Valid {
 		return key, errmsg.SUCCESS
 	}
 	return nil, errmsg.ERROR
