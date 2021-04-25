9261e60e15267e665e238fe3acffdb773106ffca
tzr2020 <zhonziran@gmail.com>
2021-04-25 15:12:22 +0800

    跨越请求中间件

---
+++ middleware/cors.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+// 跨越请求中间件
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		cors.New(cors.Config{
+			AllowOrigins:  []string{"*"},
+			AllowMethods:  []string{"*"},
+			AllowHeaders:  []string{"Origin"},
+			ExposeHeaders: []string{"Content-Length", "Authorization"},
+			MaxAge:        12 * time.Hour,
+		})
+	}
+}
